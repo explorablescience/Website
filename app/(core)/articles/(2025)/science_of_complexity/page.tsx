a07a08c71e5bc0e64f05edc6bda8f6d3b46f9948
Maxime D. <xam4lor@gmail.com>
2025-09-07 22:37:00 +0200

    Added image for article science of complexity

--- app/(core)/articles/(2025)/science_of_complexity/page.tsx
+++ app/(core)/articles/(2025)/science_of_complexity/page.tsx
@@ -4,9 +4,40 @@ import { Metadata } from "next";
 import React from "react";
 import './page.module.css'
 import ScienceComplexityApp from "./ScienceComplexityApp";
+import { getArticleById } from "@/app/api/database/articles";
 
-export const metadata: Metadata = {
-    title: 'The Science of Complexity'
+export async function generateMetadata(): Promise<Metadata> {
+    const article = await getArticleById("science_of_complexity");
+    if (!article) {
+        return {}
+    }
+    const title = `${article.title} | ExplorableScience`
+    const desc = article.description.replace(/<c>(.*?)<\/c>/g, '$1');
+
+    return {
+        title,
+        description: desc,
+        keywords: [
+            "science", "physics", "educational", "explorable", "explorables", "explorable-explanations", "interactive", "article",
+            "articles", "simulation", "simulations", "learning", "research", "experiments", "visualization", "data", "technology",
+            article.keyword
+        ],
+        openGraph: {
+            title,
+            description: desc,
+            url: `/articles/${article.id}`,
+            images: [
+                { url: article.image.url, width: article.image.width, height: article.image.height, alt: article.image.alt },
+            ],
+            siteName: 'ExplorableScience',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description: desc,
+            images: [article.image]
+        }
+    }
 }
 
 export default function Page() {
