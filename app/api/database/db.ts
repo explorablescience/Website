5dcf0e8ecdb6c9e779547dd3e3e460ddaff36086
Maxime D. <xam4lor@gmail.com>
2025-10-11 19:06:26 +0200

    Updated API key logging

--- app/api/database/db.ts
+++ app/api/database/db.ts
@@ -1,14 +1,19 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+
+console.log('FIREBASE_PROJECT_ID:', projectId);
+console.log('FIREBASE_CLIENT_EMAIL:', clientEmail);
+console.log('FIREBASE_PRIVATE_KEY:', privateKey);
 
 if (!getApps().length) {
     if (!projectId || !clientEmail || !privateKey) {
         console.warn('[firebase] Missing credentials, Firestore not initialized.\n' +
-            `FIREBASE_PROJECT_ID=${!!projectId}, FIREBASE_CLIENT_EMAIL=${!!clientEmail}, FIREBASE_PRIVATE_KEY=${!!privateKey}`);
+            `FIREBASE_PROJECT_ID=${!!projectId}, FIREBASE_CLIENT_EMAIL=${!!clientEmail}, FIREBASE_PRIVATE_KEY=${!!
+                privateKey}`);
     } else {
         try {
             initializeApp({
